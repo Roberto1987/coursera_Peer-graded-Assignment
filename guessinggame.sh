ac9f8d98254a368431f8acb375b365d199a0a5a1
root <root@rtesta-t470s.net.amelco.local>
2018-08-23 17:45:05 +0100

    adding a function in the script

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,13 +4,19 @@ echo '####### WELCOME TO GUESS-IN-GAME #######'
 echo '########################################'
 echo ' '
 echo ' '
+
+function compute_num_of_files {
+	n_of_files=$(ls -l| wc -l)
+	}
+
+
 n_of_files=-1
 while [[ ${n} -ne ${n_of_files} ]]
 do
     echo ' '
     echo 'How many files are in the current directory? please type a guess'
     read n
-    n_of_files=$(ls -l| wc -l)
+    compute_num_of_files
     	if [[ ${n} -gt ${n_of_files} ]] 
     		then echo 'Your guess is greater than the number of files in this directory'
     		elif [[ ${n} -lt ${n_of_files} ]] 
